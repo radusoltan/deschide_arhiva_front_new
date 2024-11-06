fb81118ceb10d7f8fc6681539502a1cead93153a
Radu Soltan <radusoltan@gmail.com>
2024-11-06 15:46:36 +0200

    adsense; updated elastic fields'; 7

--- app/sitemap.js
+++ app/sitemap.js
@@ -22,33 +22,33 @@ export default async function sitemap(){
     }
   ]
 
-  // let response = await client.search({
-  //   index: 'articles',
-  //   scroll: '30s',
-  //   size: 10000,
-  //   _source: ['slug','category','published_at','language'],
-  //   query: {
-  //     match_all: {}
-  //   }
-  // })
-  //
-  // while (response.hits.hits.length){
-  //   response.hits.hits.forEach(article=>{
-  //     article._source.language !== 'ro' ?
-  //         urls.push({
-  //           url: `${process.env.NEXT_PUBLIC_APP_URL}${article._source.language}/${article._source.category.slug}/${article._source.slug}`,
-  //           lastModified: article._source.published_at
-  //         }) :
-  //         urls.push({
-  //           url: `${process.env.NEXT_PUBLIC_APP_URL}${article._source.category.slug}/${article._id}/${article._source.slug}`,
-  //           lastModified: article._source.published_at
-  //         })
-  //   })
-  //   response = await client.scroll({
-  //     scroll: '30s',
-  //     scroll_id: response._scroll_id
-  //   })
-  // }
+  let response = await client.search({
+    index: 'articles',
+    scroll: '30s',
+    size: 10000,
+    _source: ['slug','category','published_at','language'],
+    query: {
+      match_all: {}
+    }
+  })
+
+  while (response.hits.hits.length){
+    response.hits.hits.forEach(article=>{
+      article._source.language !== 'ro' ?
+          urls.push({
+            url: `${process.env.NEXT_PUBLIC_APP_URL}${article._source.language}/${article._source.category.slug}/${article._source.slug}`,
+            lastModified: article._source.published_at
+          }) :
+          urls.push({
+            url: `${process.env.NEXT_PUBLIC_APP_URL}${article._source.category.slug}/${article._id}/${article._source.slug}`,
+            lastModified: article._source.published_at
+          })
+    })
+    response = await client.scroll({
+      scroll: '30s',
+      scroll_id: response._scroll_id
+    })
+  }
 
   return urls
 
